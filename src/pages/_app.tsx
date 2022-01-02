34bbe2c38811c3871ba03cf2806bd2281e3d0b6d
adelnizamutdinov <stiggpwnz@gmail.com>
2022-01-02 11:17:44 +0300

    amen

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import 'simpledotcss/simple.min.css'
 
 import type {AppProps} from 'next/app'
 
+// noinspection JSUnusedGlobalSymbols
 export default function MyApp({Component, pageProps}: AppProps) {
   return <Component {...pageProps} />
 }
